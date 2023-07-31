47c7808f1359ef56586462f4613183c350910863
Felix Valdez <fdvaldez14@gmail.com>
2023-07-31 13:02:28 +0200

    creazione cartella ENVIRONMENTS
    inserimento di apiBaseUrl: 'http://localhost:8080' + modifiche al file App.Component

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http'; // Aggiunto HttpClient
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Employee } from './employee';
 
 @Injectable({
     providedIn: 'root' // correzione da "provideIn" a "providedIn"
 })
-export class EmloyeeService {
-    private apiServerUrl = '';
+export class EmployeeService {
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient) {
 
@@ -15,21 +16,21 @@ export class EmloyeeService {
 
     // GET
     public getEmployees(): Observable<Employee[]> {
-        return this.http.get<Employee[]>('${this.apiServerUrl}/employee/all');
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`); //! attenzione: vanno aggiunti i BACKTICK
     }
 
     // POST
     public addEmployees(employee: Employee): Observable<Employee[]> {
-        return this.http.post<Employee[]>('${this.apiServerUrl}/employee/add', employee); // Corretto in post
+        return this.http.post<Employee[]>(`${this.apiServerUrl}/employee/add`, employee); // Corretto in post
     }
 
     // PUT
     public updateEmployees(employee: Employee): Observable<Employee[]> {
-        return this.http.put<Employee[]>('${this.apiServerUrl}/employee/update', employee); // Corretto in put
+        return this.http.put<Employee[]>(`${this.apiServerUrl}/employee/update`, employee); // Corretto in put
     }
 
     // DELETE
     public deleteEmployees(employeeId: number): Observable<Employee[]> {
-        return this.http.delete<Employee[]>('${this.apiServerUrl}/employee/delete/${employeeId}'); // Corretto in delete e aggiunto employeeId come parametro
+        return this.http.delete<Employee[]>(`${this.apiServerUrl}/employee/delete/${employeeId}`); // Corretto in delete e aggiunto employeeId come parametro
     }
 }
